019d3ee55ca108c67cfc72114cfb6ab8656d0237
Strating, Michael H <michael.strating@dematic.com>
2020-02-14 14:10:44 -0500

    added basic db interraction

--- delvr-api.go
+++ delvr-api.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/mikeStr8s/delvr-api/db"
 	"github.com/savsgio/atreugo/v10"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+type Ability struct {
+	Name string
+}
 
 func main() {
 	config := &atreugo.Config{Addr: "0.0.0.0:8000"}
@@ -10,7 +20,16 @@ func main() {
 
 	api := server.NewGroupPath("/api")
 	api.GET("/", func(ctx *atreugo.RequestCtx) error {
-		return ctx.TextResponse("API")
+		database := db.ConnectDatabase()
+		abilities := database.Collection("ability")
+		filter := bson.D{{"name", "Strength"}}
+
+		var result Ability
+		err := abilities.FindOne(context.TODO(), filter).Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return ctx.TextResponse(fmt.Sprintf("Found Single Document: %+v\n", result))
 	})
 
 	if err := server.ListenAndServe(); err != nil {
